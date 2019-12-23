48662a077d835a86bb91582bb38a19907152fe3d
Marcel Benders <marcel.benders@icloud.com>
2019-12-23 02:12:16 +0100

    (dev) Update caching

--- Slipways.Data/Contracts/IWaterRepository.cs
+++ Slipways.Data/Contracts/IWaterRepository.cs
@@ -1,8 +1,13 @@
-﻿using com.b_velop.Slipways.Data.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using com.b_velop.Slipways.Data.Models;
 
 namespace com.b_velop.Slipways.Data.Contracts
 {
     public interface IWaterRepository : IRepositoryBase<Water>
     {
+        Task<ILookup<Guid, Water>> GetWatersByIdAsync(IEnumerable<Guid> waterIds);
     }
 }
--- Slipways.Data/Repositories/WaterRepository.cs
+++ Slipways.Data/Repositories/WaterRepository.cs
@@ -1,4 +1,8 @@
-﻿using com.b_velop.Slipways.Data.Contracts;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using com.b_velop.Slipways.Data.Contracts;
 using com.b_velop.Slipways.Data.Helper;
 using com.b_velop.Slipways.Data.Models;
 using Microsoft.Extensions.Caching.Distributed;
@@ -14,6 +18,14 @@ namespace com.b_velop.Slipways.Data.Repositories
              ILogger<RepositoryBase<Water>> logger) : base(db, dcache, logger)
         {
             Key = Cache.Waters;
+        }
+
+        public async Task<ILookup<Guid, Water>> GetWatersByIdAsync(
+            IEnumerable<Guid> waterIds)
+        {
+            var waters = await SelectAllAsync();
+            var result = waters.Where(_ => waterIds.Contains(_.Id));
+            return result.ToLookup(x => x.Id);
         }
     }
 }

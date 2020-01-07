92b09817f0af4e38a3ec1f155576e19fbf9e0008
Marcel Benders <marcel.benders@cleverbridge.com>
2020-01-07 18:08:06 +0100

    (dev) Upate Models

--- Slipways.Data/Models/Port.cs
+++ Slipways.Data/Models/Port.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace com.b_velop.Slipways.Data.Models
@@ -7,12 +8,28 @@ namespace com.b_velop.Slipways.Data.Models
     [Serializable]
     public class Port : Locationable
     {
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Hompage")]
+        [Url]
         public string Url { get; set; }
+
+        [Display(Name = "Straße")]
         public string Street { get; set; }
+
+        [Display(Name = "PLZ")]
         public string Postalcode { get; set; }
+
+        [Display(Name = "Stadt / Ort")]
         public string City { get; set; }
+
+        [Display(Name = "Telefon")]
+        [Phone]
         public string Phone { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress]
         public string Email { get; set; }
 
         //[NotMapped]

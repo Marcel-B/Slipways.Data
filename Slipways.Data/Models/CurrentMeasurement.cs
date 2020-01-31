8ce651bb4b29d2c41e6aeaf9f6826c3dd4e69b37
Marcel Benders <marcel.benders@cleverbridge.com>
2020-01-31 17:16:40 +0100

    (dev) Add Measurement Model

---
+++ Slipways.Data/Models/CurrentMeasurement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace com.b_velop.Slipways.Data.Models
+{
+    [Serializable]
+    public class CurrentMeasurement : Entity
+    {
+        public DateTime Timestamp { get; set; }
+        public double Value { get; set; }
+        public int Trend { get; set; }
+        public string StateMnwMhw { get; set; }
+        public string StateNswHsw { get; set; }
+
+        [ForeignKey("StationFk")]
+        public Station Station { get; set; }
+
+        public Guid StationFk { get; set; }
+    }
+}
